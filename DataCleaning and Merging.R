9dd29c5f61ae42127b33276f5a988040e9da5e31
Rushikesh Maheshwari <rushikesh.maheshwari12@gmail.com>
2019-07-26 17:20:28 -0400

    model building

--- DataCleaning and Merging.R
+++ DataCleaning and Merging.R
@@ -25,6 +25,23 @@ demographic$literacy <- as.numeric(gsub("\\%","",demographic$literacy)) #removin
 demographic$population <- as.numeric(gsub("\\,","",demographic$population)) #removing comma in population data
 demographic <- transform(demographic, sex.ratio = as.numeric(sex.ratio), density = as.numeric(density))
 
+#Reading Unemployment Data
+unemp <-read.xlsx("Masterdata.xlsx", sheet = "UnempData")
+str(unemp)
+names(unemp)[1]<- "state"
+names(unemp)[2]<- "rate"
+unemp <- unemp[-c(1,29),]
+unemp$rate <- as.numeric(unemp$rate)
+
+#Reading MFI Growth Data
+mfigrowth <-read.xlsx("Masterdata.xlsx", sheet = "MFIGrowth")
+str(mfigrowth)
+names(mfigrowth)[1]<- "state"
+names(mfigrowth)[2]<- "count.shg"
+names(mfigrowth)[3]<- "saving.amount.lakh.rs"
+
+unemp$rate <- as.numeric(unemp$rate)
+
 #Reading State wise branchList
 branch <- import("State and District wise branch list.xlsx")
 str(branch)
@@ -58,6 +75,8 @@ head(datafinal)
 #Combining demographic data and Branch level profit data
 
 finaldata <- left_join(datafinal,branch_pl, by = "branch")
+finaldata <- left_join(finaldata,unemp, by = "state")
+finaldata <- left_join(finaldata,mfigrowth,by ="state")
 head(finaldata)
 finaldata <- finaldata[,-c(1,11)]
 str(finaldata)
@@ -67,18 +86,55 @@ original <- finaldata
 
 #Imputing missing data for branches not mentioned in previous list
 finaldata[sapply(finaldata, is.numeric)] <- lapply(finaldata[sapply(finaldata, is.numeric)], function(x) ifelse(is.na(x), median(x, na.rm = TRUE), x))
-finaldata$result <- ifelse(finaldata$finalvalue > 0,"Profit","Loss")
+finaldata$result <- as.factor(ifelse(finaldata$finalvalue > 0,"Profit","Loss"))
 write.csv(finaldata, file = "Finaldataforanalysis.csv")
 
-m1 = glm(as.factor(result)~ density+sex.ratio+literacy+increase,family=binomial(link = "logit"), data = finaldata)
+m1 = glm(result~ density+sex.ratio+literacy+increase,family=binomial(link = "logit"), data = finaldata)
+m2 = glm(result~ density+sex.ratio+literacy+increase+rate,family=binomial(link = "logit"), data = finaldata)
+m3 = glm(result~ density+sex.ratio+literacy+increase+rate+finaldata$count.shg,family=binomial(link = "logit"), data = finaldata)
+m4 = glm(result~ (density*rate)+sex.ratio+literacy+increase+rate,family=binomial(link = "logit"), data = finaldata)
+m5 = glm(result~ density+sex.ratio+literacy*rate+increase,family=binomial(link = "logit"), data = finaldata)
+m6 = glm(result~ density+sex.ratio+literacy+increase*rate,family=binomial(link = "logit"), data = finaldata)m7 = glm(result~ density+sex.ratio+literacy*increase*rate,family=binomial(link = "logit"), data = finaldata)
+m7 = glm(result~ density+sex.ratio+literacy,family=binomial(link = "logit"), data = finaldata)
+
+
+library(stargazer)
+
+
+stargazer(m1,m2,m3,type = "text")
+
+summary(m7)
+
 fitted.values(m1)
 m1$fitted.values
 confu
 summary(m1)
 plot(m1)
+rep
 
 #Creating Test Data Set
 
-test_data <- subset(finaldata, district == "Bhagalpur") #to be extracted from demographic file as the branch is not present in Finaldata
+test_data <- subset(demographic, district %in% c("BHAGALPUR","SAHARSA","KHAGARIA","BADARPUR","KARIMGANJ","CACHAR")) #to be extracted from demographic file as the branch is not present in Finaldata
+#predict.glm(test_data,m1)
+head(test_data)
 
+exvalue <- function(x)
+  {
+  y <- exp(-x)/(1+(exp(-x)))
+  return(y)
+}
+  
+exvalue(m1$coefficients)
+
+test.prob = predict(m1, test_data, type="response")
+
+
+test_pred_num <- ifelse(test.prob > 0.5, "Profit", "Loss")
+y_pred <- factor(test_pred_num, levels=c("Loss", "Profit"))
+y_act <- test_data$
+mean(y_pred == y_act)
+
+test.predict = rep("Loss", dim(finaldata)[12])
+test.predict[test.prob > .5] = "Profit"
+table(test.pred, as.factor(finaldata$result))
 
